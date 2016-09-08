8bd15cea9f3a47687ec263ed9a3eac6310b3e6d1
Remi Rampin <remirampin@gmail.com>
2016-09-08 13:07:11 -0400

    Add conrod UI

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ extern crate piston;
 extern crate piston_window;
 extern crate graphics;
 extern crate glutin_window;
+#[macro_use] extern crate conrod;
+
+use std::path::Path;
 
 use piston::window::{AdvancedWindow, WindowSettings};
 use piston_window::{Context, G2d, OpenGL, PistonWindow};
 use piston::event_loop::*;
 use piston::input::*;
 use glutin_window::GlutinWindow;
+use conrod::{Labelable, Positionable, Sizeable, Widget};
 
 pub struct App {
     rotation: f64,  // Rotation for the square.
@@ -20,7 +24,7 @@ impl App {
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        let square = rectangle::square(0.0, 0.0, 50.0);
+        let square = rectangle::square(0.0, 0.0, 150.0);
         let rotation = self.rotation;
         let (x, y) = (100.0, 100.0);
 
@@ -29,7 +33,7 @@ impl App {
 
         let transform = c.transform.trans(x, y)
                                    .rot_rad(rotation)
-                                   .trans(-25.0, -25.0);
+                                   .trans(-75.0, -75.0);
 
         // Draw a box rotating around the middle of the screen.
         rectangle(RED, square, transform, g);
@@ -42,16 +46,18 @@ impl App {
 }
 
 fn main() {
+    const WIDTH: u32 = 200;
+    const HEIGHT: u32 = 200;
+
     // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
     let mut window: PistonWindow<GlutinWindow> = WindowSettings::new(
             "fluffy-fiesta",
-            [200, 200]
+            [WIDTH, HEIGHT]
         )
         .opengl(opengl)
-        .exit_on_esc(true)
         .build()
         .unwrap();
 
@@ -60,17 +66,79 @@ fn main() {
         rotation: 0.0,
     };
 
+    // Construct our `Ui`.
+    let mut ui = conrod::UiBuilder::new().build();
+
+    // Generate the widget identifiers.
+    widget_ids!(struct Ids { canvas, resume, quit });
+    let ids = Ids::new(ui.widget_id_generator());
+
+    // Add a `Font` to the `Ui`'s `font::Map` from file.
+    let font_path = Path::new("assets/NotoSans-Regular.ttf");
+    assert!(font_path.exists());
+    ui.fonts.insert_from_file(font_path).unwrap();
+
+    // Create a texture to use for efficiently caching text on the GPU.
+    let mut text_texture_cache =
+        conrod::backend::piston_window::GlyphCache::new(&mut window, WIDTH, HEIGHT);
+
+    // The image map describing each of our widget->image mappings (in our case, none).
+    let image_map = conrod::image::Map::new();
+
     let mut capture = false;
+    let mut ui_on = false;
 
     let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
         // Draw the app and the ui.
         window.draw_2d(&e, |c, g| {
             app.render(&c, g);
+
+            if ui_on {
+                let primitives = ui.draw();
+                fn texture_from_image<T>(img: &T) -> &T { img };
+                conrod::backend::piston_window::draw(c, g, primitives,
+                                                     &mut text_texture_cache,
+                                                     &image_map,
+                                                     texture_from_image);
+            }
         });
+
+        // Convert the piston event to a conrod event.
+        if let Some(ce) = conrod::backend::piston_window::convert_event(e.clone(), &window) {
+            ui.handle_event(ce);
+        }
 
         if let Some(u) = e.update_args() {
             app.update(&u);
+
+            if ui_on {
+                let ui = &mut ui.set_widgets();
+
+                // Create a background canvas upon which we'll place the button.
+                conrod::widget::Canvas::new().floating(true).w_h(100.0, 70.0).pad(10.0).middle().set(ids.canvas, ui);
+
+                // Draw the buttons.
+                if conrod::widget::Button::new()
+                    .mid_top_of(ids.canvas)
+                    .w_h(80.0, 20.0)
+                    .label("Resume")
+                    .set(ids.resume, ui)
+                    .was_clicked()
+                {
+                    ui_on = false;
+                }
+
+                if conrod::widget::Button::new()
+                    .down(10.0)
+                    .w_h(80.0, 20.0)
+                    .label("Quit")
+                    .set(ids.quit, ui)
+                    .was_clicked()
+                {
+                    break;
+                }
+            }
         }
 
         if let Some(Button::Mouse(button)) = e.press_args() {
@@ -79,7 +147,14 @@ fn main() {
 
         if let Some(Button::Keyboard(key)) = e.press_args() {
             println!("Pressed key '{:?}'", key);
-            if key == Key::C {
+            if key == Key::Escape {
+                println!("ui on");
+                ui_on = true;
+                if capture {
+                    capture = false;
+                    window.set_capture_cursor(false);
+                }
+            } else if !ui_on && key == Key::C {
                 capture = !capture;
                 window.set_capture_cursor(capture);
                 println!("capture {}", if capture { "on" } else { "off" });
